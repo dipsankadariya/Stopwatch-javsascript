a89f4d500d02d0a1637cd771b10796daf42db9cc
Dipsan Kadariya <139572157+dipsankadariya@users.noreply.github.com>
2024-03-04 19:34:40 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const timerDisplay = document.querySelector('.timer-display');
+const startButton = document.querySelector('.js-start-button');
+const pauseButton = document.querySelector('.js-pause-button');
+const resetButton = document.querySelector('.js-reset-button');
+
+let timer;
+let running = false;
+let time = 0;
+
+function updateTimer() {
+  const hours = Math.floor(time / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+ 
+  timerDisplay.textContent = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+startButton.addEventListener('click', () => {
+  if (!running) {
+    timer = setInterval(() => {
+      time += 1000;
+      updateTimer();
+    }, 1000);
+    running = true;
+  }
+});
+
+pauseButton.addEventListener('click', () => {
+  clearInterval(timer);
+  running = false;
+});
+
+resetButton.addEventListener('click', () => {
+  clearInterval(timer);
+  running = false;
+  time = 0;
+  updateTimer();
+});
+
